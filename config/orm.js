0dac73d41c73fab53422ff93a829393c4dccb6b2
Patience D <patience.deaton2@outlook.com>
2019-06-01 12:16:22 -0400

    test

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ let connection = require("../config/connection.js");
 var orm = {
     
     selectAll: function (callback) {
-        connection.query('SELECT * FROM ??', function (err, result) {
+        connection.query('SELECT * FROM burgers', function (err, result) {
             if (err) throw err;
             callback(result);
             console.log(result);
@@ -11,7 +11,7 @@ var orm = {
     },
 
     insertOne: function (burger_name, callback) {
-        connection.query('INSERT INTO ?? SET ?', {
+        connection.query('INSERT INTO ? SET ?', {
             burger_name: burger_name,
             devoured: false,
         }, function (err, result) {
@@ -22,7 +22,7 @@ var orm = {
     },
 
     updateOne: function (burgerID, callback) {
-        connection.query('UPDATE ?? SET ? WHERE ?', [{ devoured: true }, { id: burgerID }],
+        connection.query('UPDATE ? SET ? WHERE ?', [{ devoured: true }, { id: burgerID }],
             function (err, result) {
                 if (err) throw err;
                 callback(result);
